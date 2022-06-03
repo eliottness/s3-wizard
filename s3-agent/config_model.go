53252bdb2534331c9a21b891c09539aae0eb6553
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-06-03 16:53:35 +0200

    feat(s3-agent): added config model
    Signed-off-by: Eliott Bouhana <eliott.bouhana@epita.fr>

---
+++ s3-agent/config_model.go
@@ -0,0 +1,71 @@
+package main
+
+import "net/url"
+
+const (
+    // Basic rule, when the file is updated, send it to the backend
+    // no parameters
+    REPLICATE = "REPLICATE"
+
+    // if the source is older than X
+    // send it to the backend and leave a dummy behind which will download the source upon opening
+    // Parameters example: { value = "3", "unit" = "days" }
+    OLDER_THAN = "OLDER_THAN"
+
+    // if the source is newer than X
+    // Parameters example: { value = "1", "unit" = "week" }
+    NEWER_THAN = "NEWER_THAN"
+
+    // Send the file when larger than X
+    // Parameters example: { value = "1", "unit" = "Go" }
+    LARGER_THAN = "LARGER_THAN"
+
+    // Send the file when smaller than X
+    // Parameters example: { value = "496", "unit" = "Mo" }
+    SMALLER_THAN = "SMALLER_THAN"
+
+    // User if the file is X
+    // Parameters example: "john"
+    USER_IS = "USER_IS"
+)
+
+type RuleType string
+
+type ValueTypeParamater struct {
+    // must be positive
+    value   int
+    // can be:
+    // * years, months, weeks, days, hours, minutes, seconds    -> time rules
+    // * To, Go, Mo, Ko                                         -> size rules
+    unit    string
+}
+
+type Server struct {
+    Url url.URL     `json:"url"`        // url of the server to connect to
+    User string     `json:"user"`       // user to connect to the server
+    Password string `json:"password"`   // password to connect to the server
+}
+
+type Rule struct {
+    // type of rule, must be in the elements above
+    Type    RuleType        `json:"type"`
+
+    // paramaters for the rule
+    Params interface{}      `json:"params"`
+
+    // source path: can be a file or a folder in the local filesystem
+    // if the source is a file, apply the rule
+    // if the source is a folder, apply the rule on all its files
+    // support shell globbing
+    Src     string          `json:"src"`
+
+    // destination path: must be a valid server name
+    Dest    string          `json:"dest"`
+}
+
+type Config struct {
+    Servers map[string]Server   `json:"servers"`            // servers to connect to
+    Rules   []Rule              `json:"rules"`              // rules to apply
+    ExcludePatterns []string    `json:"exclude_patterns"`   // exclude files matching this paterns
+
+}
--- s3-agent/go.mod
+++ s3-agent/go.mod
@@ -3,3 +3,5 @@ module gitlab.softalys.com/eliott/s3-agent
 go 1.18
 
 require github.com/alecthomas/kong v0.5.0
+
+require github.com/pkg/errors v0.9.1 // indirect
