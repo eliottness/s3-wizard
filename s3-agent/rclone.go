69b305d5e4658c90391501af7c59a7949b994232
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-06-18 16:47:57 +0200

    finished rclone api
    Signed-off-by: Eliott Bouhana <eliott.bouhana@epita.fr>

--- s3-agent/rclone.go
+++ s3-agent/rclone.go
@@ -19,6 +19,7 @@ func memfdCreate(path string) (r1 uintptr, err error) {
         return 0, err
     }
 
+    // https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
     r1, _, errno := syscall.Syscall(319, uintptr(unsafe.Pointer(s)), 0, 0)
 
     if int(r1) == -1 {
@@ -55,29 +56,44 @@ func execveAt(fd uintptr, args []string) (err error) {
         return errno
     }
 
-    // never hit
-    log.Println("should never hit")
+    // should never hit
+    log.Println("Error in execveAt")
     return err
 }
 
 /// Run the rclone binary with the given arguments.
 /// The rclone binary is embed in our main binary.
 /// This function create a memory space associated with a file descriptor.
+/// It copies the rclone binary to the memory space.
 /// This file descriptor is passed to execvp with the arguments to run rclone
-func RunRClone(args []string) error {
+func RunRClone(args []string) (int, error) {
+
+    syscall.ForkLock.Lock()
+
+    id, _, _ := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+    if id != 0 {
+        syscall.ForkLock.Unlock()
+        var wstatus syscall.WaitStatus;
+        syscall.Wait4(int(id), &wstatus, 0, nil)
+
+        return int(wstatus.ExitStatus()), nil
+    }
+
     fd, err := memfdCreate("/file.bin")
     if err != nil {
-        return err
+        return -1, err
     }
 
     if err = copyToMem(fd, rcloneBinary); err != nil {
-        return err
+        return -1, err
     }
 
     if err = execveAt(fd, args); err != nil {
-        return err
+        return -1, err
     }
 
-    return nil
+    // Should never reach
+    log.Println("Error in RunRClone")
+    return -1, nil
 }
 
