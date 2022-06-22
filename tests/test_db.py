7156b0abd70b548b05257da5bc976777e70eefda
Alexandre Rulleau <rulleaualexandre@gmail.com>
2022-06-22 20:32:41 +0200

    feat(tests): add tests setup and teardown

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,4 @@
+from cProfile import run
 import os
 import pytest
 import sqlite3
@@ -7,34 +8,42 @@ CHECK_REF = False
 BINARY = 's3-agent/s3-agent'
 CONFIG_PATH = 'tests/data/'
 
-def run_minio():
-    return subprocess.run('docker run --name dev-s3 -e MINIO_ROOT_USER=minioadmin -e MINIO_ROOT_PASSWORD=minioadmin -p 9000:9000 -p 9001:9001 minio/minio:latest server --console-address ":9001"', stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+class TestS3AgentClass():
 
-def stop_minio():
-    return subprocess.run('docker stop dev-s3')
+    def run_command(cmd, stdout=None, stderr=None, code=None):
+        process = subprocess.run(cmd.split(' '), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        assert True if stdout is None else process.stdout.decode() == stdout
+        assert True if stderr is None else process.stderr.decode() == stderr
+        assert True if code is None else process.returncode == code
 
-def run_command(cmd):
-    process = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    return process
+    def setup_class(self):
+        cmd = 'docker run -d --name dev-s3 -e MINIO_ROOT_USER=minioadmin -e MINIO_ROOT_PASSWORD=minioadmin -p 9000:9000 -p 9001:9001 minio/minio:latest server /data --console-address ":9001"'
+        self.run_command(cmd, code=0)
 
-def test_simple_file():
-    filename = 'test.txt'
-    config_name = 'simpleconfig.json'
-    tmp_folder_name = 'tmp'
-    db_path = 'db/'
+    def teardown_class(self):
+        self.run_command('docker rm -f dev-s3', code=0)
 
-    print(os.system('ls'))
+    def setup_method(self):
+        pass
 
-    run_minio()
+    def teardown_method(self):
+        pass
 
-    # Starting the test
-    os.mkdir(tmp_folder_name)
-    os.mknod(tmp_folder_name + filename)
+    def test_simple_file(self):
+        # Todo
+        pass
+        # filename = 'test.txt'
+        # config_name = 'simpleconfig.json'
+        # tmp_folder_name = 'tmp'
+        # db_path = 'db/'
 
-    process = run_command(f'./{BINARY} --config-folder={CONFIG_PATH} sync')
+        # # Starting the test
+        # os.mkdir(tmp_folder_name)
+        # os.mknod(tmp_folder_name + filename)
 
-    # Connect to the DB to check its content
-    # con = sqlite3.connect('example.db')
+        # process = run_command(f'./{BINARY} --config-folder={CONFIG_PATH} sync')
 
-    os.rmdir(tmp_folder_name)
-    stop_minio()
+        # # Connect to the DB to check its content
+        # # con = sqlite3.connect('example.db')
+
+        # os.rmdir(tmp_folder_name)
