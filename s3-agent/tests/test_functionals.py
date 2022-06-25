e27030cf6bc5e8545b2e76a8b9e7b4c0174e33dd
Alexandre Rulleau <rulleaualexandre@gmail.com>
2022-06-25 15:56:06 +0200

    feat(tests): Add test with two files with same name

--- s3-agent/tests/test_functionals.py
+++ s3-agent/tests/test_functionals.py
@@ -144,3 +144,20 @@ class TestS3AgentClass:
             assert file.readlines()[0] == 'Hello world'
 
         assert_entry_state(handle_agent, 'test_simple_file.txt', 11, 1, '')
+
+    def test_same_name_files(self, handle_agent):
+        ### GIVEN ###
+        os.mkdir(f'{FILESYSTEM_PATH}/test_simple_folder')
+        with open(f'{FILESYSTEM_PATH}/test_simple_folder/test_simple_file.txt', 'w') as file:
+            file.write('Hello world')
+        with open(f'{FILESYSTEM_PATH}/test_simple_file.txt', 'w') as file:
+            file.write('Hello world 2')
+
+        ### WHEN ###
+        time.sleep(5)
+
+        ### THEN ###
+        with open(f'{FILESYSTEM_PATH}/test_simple_folder/test_simple_file.txt') as file:
+            assert file.readlines()[0] == 'Hello world'
+        with open(f'{FILESYSTEM_PATH}/test_simple_file.txt') as file:
+            assert file.readlines()[0] == 'Hello world 2'
