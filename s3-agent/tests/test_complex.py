ecaa3e575ff482e16897d0a26040815409e82428
Alexandre Rulleau <rulleaualexandre@gmail.com>
2022-06-26 04:33:53 +0200

    feat(tests): Add restart test

--- s3-agent/tests/test_complex.py
+++ s3-agent/tests/test_complex.py
@@ -29,3 +29,29 @@ class TestS3AgentClassComplex:
 
         # Reset testing environment
         stop_agent(process, connection)
+
+
+    def test_restart(self):
+        ### GIVEN ###
+        process, connection = start_agent('tests/data/simple_config.json')
+
+        first_file_path = 'test_simple_file.txt'
+        second_file_path = 'folder/test_simple_file.txt'
+        first_content = 'Hello world first'
+        second_content = 'Hello world second'
+
+        create_file(first_file_path, first_content, agent=False)
+        create_file(second_file_path, second_content, agent=False)
+
+        ### WHEN ###
+        time.sleep(2)
+        stop_agent(process, connection, reset_env=False)
+        process, connection = start_agent('tests/data/simple_config.json', reset_env=False)
+        time.sleep(2)
+
+        ### THEN ###
+        assert_agent_file(connection.cursor(), first_file_path, first_content)
+        assert_agent_file(connection.cursor(), second_file_path, second_content)
+
+        # Reset testing environment
+        stop_agent(process, connection)
